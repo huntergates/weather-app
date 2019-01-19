0e322bb76a20a68812960a4fdcc7638ab37d52e2
Sofia Gates <sofiasgates@gmail.com>
2019-01-19 12:39:14 -0600

    Initial Commit

---
+++ weather-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Hunter Gates on 1/19/19.
+//  Copyright © 2019 Hunter Gates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
